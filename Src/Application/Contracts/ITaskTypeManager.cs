583da8cf7f38a32d7d283d077f8f703465ce1ca9
avin.smd <avin.smd@gmail.com>
2025-05-25 23:50:26 +0330

    feat(Application): add interface for task type use case

---
+++ Src/Application/Contracts/ITaskTypeManager.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Application.Dtos;
+using Domain;
+using Domain.Contracts.Data;
+using Domain.ResultPattern;
+
+namespace Application.Contracts
+{
+    public interface ITaskTypeManager
+    {
+        Task<Result<Guid>> CreateTaskAsync(CreateTaskDto createTaskDto);
+        Task<Result> UpdateTaskAsync(UpdateTaskDto updateTaskDto);
+        Task<Result> DeleteTaskAsync(Guid TaskId);
+        Task<Result<GetTaskDto>> GetTaskByIdAsync(Guid TaskId);
+        Task<Result<PagedList<TaskDto>>> SearchTaskAsync(SearchTaskDto searchTaskDto);
+    }
+}
