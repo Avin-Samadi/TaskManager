ba5f23807de86f0550a331eba428d63f87bf6d28
avin.smd <avin.smd@gmail.com>
2025-05-25 23:42:59 +0330

    feat(Domain): add interface for task repository

---
+++ Src/Domain/Contracts/ITaskTypeRepository.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Domain.Contracts
+{
+    public interface ITaskTypeRepository : IRepository<TaskType, Guid> { }
+}
