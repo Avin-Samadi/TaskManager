32236cd1a406bf6f1c57da1f0f70349ef902da78
avin.smd <avin.smd@gmail.com>
2025-05-25 23:45:03 +0330

    feat(Application): add dto for update task

---
+++ Src/Application/Dtos/UpdateTaskDto.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Application.Dtos
+{
+    public record UpdateTaskDto(
+        string? Title,
+        string? Description,
+        string IsCompleted,
+        DateTime DueDate
+    );
+}
