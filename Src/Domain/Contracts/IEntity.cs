f4528118987587a5bbb2155eed1f90f9504c3e49
avin.smd <avin.smd@gmail.com>
2025-05-25 14:15:20 +0330

    feat(domain): add IEntity interface as a base entity contract

---
+++ Src/Domain/Contracts/IEntity.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Domain.Contracts
+{
+    public interface IEntity<TId>
+    {
+        TId Id { get; set; }
+    }
+}
