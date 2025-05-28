05293c7ac3afd6a5a101c6e4fed3fc625f2e46c6
avin.smd <avin.smd@gmail.com>
2025-05-28 12:20:12 +0330

    feat: register services

--- Src/Presentation/Program.cs
+++ Src/Presentation/Program.cs
@@ -1,6 +1,34 @@
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using Infrastructure.Extensions;
+using Presentation;
+using Presentation.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddInfrastructure(builder.Configuration);
+
+builder.Services.AddEndpoints(typeof(Program).Assembly);
+
+builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly);
+builder.Services.AddFluentValidationAutoValidation();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Task Manager API V1");
+    });
+}
+
+app.MapEndpoints();
 
 app.Run();
